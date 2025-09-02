ce8f228d07492b3a9ac7e19f88d2a43844e809a0
johantorosjan <johan.torosjan@etu.umontpellier.fr>
2025-09-02 12:50:37 +0200

    AB

--- speech-synthesis-frontend/ember-cli-build.js
+++ speech-synthesis-frontend/ember-cli-build.js
@@ -5,19 +5,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     fingerprint: {
-      enabled: true,
-      // Ne pas exclure les fingerprints, mais gérer SRI différemment
-      generateAssetMap: true
+      exclude: ['assets/**/*']
     },
-    sri: {
-      enabled: true,
-      // Excluez seulement les assets qui changent fréquemment
-      exclude: [
-        'assets/images/**/*',
-        'assets/fonts/**/*',
-        '**/speech-synthesis-frontend.js'
-      ]
-    }
+    
+    // Add options here
   });
 
   return app.toTree();
