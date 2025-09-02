67308a7479c59e2d1080de98472dda93ddd78f35
johantorosjan <johan.torosjan@etu.umontpellier.fr>
2025-09-02 11:24:35 +0200

    excludes finger

--- speech-synthesis-frontend/ember-cli-build.js
+++ speech-synthesis-frontend/ember-cli-build.js
@@ -5,7 +5,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     fingerprint: {
-      exclude: ['assets/d.png','assets/trash_image.png','assets/d.png']
+      exclude: ['assets/d.png','assets/trash_image.png','assets/d.png','assets/4.png']
     }
     // Add options here
   });
